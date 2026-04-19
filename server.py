84f0307cff2e68ba572c91dd20ccf08e00b83106
Aadesh <aadesh.maheshwari@yahoo.com>
2026-04-19 17:51:30 +0530

    Add chrome-extension support to CORS origins

--- server.py
+++ server.py
@@ -170,7 +170,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["*", "chrome-extension://*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
